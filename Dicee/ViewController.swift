6f54632b8b6536c9a55403ac1534e250bc24f4f3
Diego Ocampo <jd.ocampom0117@icloud.com>
2020-03-01 10:21:27 -0500

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Juan Diego Ocampo on 3/1/20.
+//  Copyright © 2020 Diego Ocampo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
